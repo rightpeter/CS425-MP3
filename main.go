024aa07519dcb29d871d2e0ded3ec5e868182be7
Rightpeter <rightpeter.lu@gmail.com>
2018-10-30 17:54:55 -0500

    Failure Detector

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"CS425/CS425-MP3/sdfs"
+	"flag"
+	"io/ioutil"
+	"log"
+	"os"
+	"time"
+)
+
+// This function will register and initiate server
+func main() {
+	// parse argument
+	configFilePath := flag.String("c", "./config.json", "Config file path")
+	fdConfigFilePath := flag.String("fdc", "./failure_detector.config.json", "Failure Detector Config file path")
+
+	// load config file
+	configFile, err := ioutil.ReadFile(*configFilePath)
+	if err != nil {
+		log.Fatalf("File error: %v\n", err)
+	}
+
+	// load fd config file
+	fdConfigFile, err := ioutil.ReadFile(*fdConfigFilePath)
+	if err != nil {
+		log.Fatalf("File error: %v\n", err)
+	}
+
+	// Class for server
+	s := sdfs.NewSDFS(configFile, fdConfigFile)
+
+	f, err := os.OpenFile(s.GetConfigPath(), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	defer f.Close()
+
+	log.SetOutput(f)
+
+	for {
+		err = s.JoinToGroup()
+		if err != nil {
+			log.Printf("join to group failed: %s\n", err.Error())
+			log.Printf("try to join to group 5 seconds later...")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		log.Printf("join to group successfully\n\n")
+		break
+	}
+
+	log.Printf("Starting server on IP: %s and port: %d\n\n", s.GetIP(), s.GetPort())
+	s.StartFailureDetector()
+}
---
+++ sdfs/sdfs.go
@@ -0,0 +1,79 @@
+// Package sdfs package for simple distributed file system
+package sdfs
+
+// refer to https://varshneyabhi.wordpress.com/2014/12/23/simple-udp-clientserver-in-golang/
+
+import (
+	"encoding/json"
+
+	"CS425/CS425-MP1/model"
+	failureDetector "CS425/CS425-MP2/server"
+)
+
+// SDFS SDFS class
+type SDFS struct {
+	config          model.NodeConfig
+	sortedMemList   []string // ["id-ts", ...]
+	failureDetector *failureDetector.Server
+}
+
+// NewSDFS init a SDFS
+func NewSDFS(sdfsConfig []byte, failureDetectorConfig []byte) *SDFS {
+	sdfs := &SDFS{}
+	sdfs.init(sdfsConfig, failureDetectorConfig)
+	return sdfs
+}
+
+// GetConfigPath export config file path
+func (s *SDFS) GetConfigPath() string {
+	return s.config.LogPath
+}
+
+func (s *SDFS) loadConfigFromJSON(jsonFile []byte) error {
+	return json.Unmarshal(jsonFile, &s.config)
+}
+
+func (s *SDFS) init(sdfsConfig []byte, failureDetectorConfig []byte) {
+	s.failureDetector = failureDetector.NewServer(failureDetectorConfig)
+	json.Unmarshal(sdfsConfig, &s.config)
+}
+
+// GetIP getip for server
+func (s *SDFS) GetIP() string {
+	return s.config.IP
+}
+
+// SetIP setip for SDFS
+func (s *SDFS) SetIP(IP string) {
+	s.config.IP = IP
+}
+
+// GetPort getport of SDFS
+func (s *SDFS) GetPort() int {
+	return s.config.Port
+}
+
+// GetMemberList return sortedMemList
+func (s *SDFS) GetMemberList() []string {
+	return s.sortedMemList
+}
+
+// SetPort setport for SDFS
+func (s *SDFS) SetPort(port int) {
+	s.config.Port = port
+}
+
+// JoinToGroup jointogroup
+func (s *SDFS) JoinToGroup() error {
+	err := s.failureDetector.JoinToGroup()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// StartFailureDetector StartFailureDetector
+func (s *SDFS) StartFailureDetector() {
+	go s.failureDetector.ServerLoop()
+	s.failureDetector.FailureDetection()
+}
