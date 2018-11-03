24a0ef00af769fbcf62774cff244dd29cc862635
Rightpeter <rightpeter.lu@gmail.com>
2018-11-03 15:55:11 -0500

    Fix

--- sdfs/sdfs.go
+++ sdfs/sdfs.go
@@ -298,7 +298,7 @@ func (s *SDFS) keepUpdatingMemberList() {
 
 			//log.Printf("keepUpdatingMemberList: nodesRPCclient: %v", s.nodesRPCClients)
 			//log.Printf("keepUpdatingMemberList: updated newNodes: %v, failNodes: %v", newNodes, failNodes)
-			//log.Printf("keepUpdatingMemberList: s.sortedMemList: %v", s.sortedMemList)
+			log.Printf("keepUpdatingMemberList: s.sortedMemList: %v", s.sortedMemList)
 			s.pushIndexToAll()
 		}
 	}
