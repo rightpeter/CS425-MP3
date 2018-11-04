e4a968400a3bab68a302c06735f9baf48b533f95
Rightpeter <rightpeter.lu@gmail.com>
2018-11-04 17:50:20 -0600

    Add corpus script

---
+++ corpus.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import os
+import time
+from subprocess import call
+
+
+def main():
+    total_time = 0
+    for file_name in os.listdir("./files/raw.en"):
+        print(file_name)
+        time_start = time.time()
+        call(["go", "run", "client/client.go", "-put", file_name])
+        time_end = time.time()
+
+        print("push {} used {}".format(time_end - time_start))
+        total_time += time_end - time_start
+        time.sleep(0.5)
+
+    print("total time: {}".format(total_time))
+
+
+if __name__ == '__main__':
+    main()
